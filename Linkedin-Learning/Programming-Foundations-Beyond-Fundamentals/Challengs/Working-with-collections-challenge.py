2d6808466d732477dede41f93e884fa97d445e18
EzlamElshahawi <79419183+EzlamElshahawi@users.noreply.github.com>
2021-10-15 16:55:50 +0200

    Update Working-with-collections-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
@@ -1,1 +1,41 @@
+# Nearest stars to Earth
+# make a list called stars with these variables
+#star1 = 'Sol'
+#star2 = 'Alpha Centauri'
+#star3 = 'Barnard'
+#star4 = 'Wolf 359'
 
+stars = [
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359'
+]
+
+# Highest peak on each tectonic plate
+# Make a dictionary called peaks that stores all values
+#African = 'Kilimanjaro'
+#Antarctic = 'Vinson'
+#Australian = 'Puncak Jaya'
+#Eurasian = 'Everest'
+#North_American = 'Denali'
+#Pacific = 'Mauna Kea'
+#South_American = 'Aconcagua'
+
+peaks = {
+    'African': 'Kilimanjaro',
+    'Antarcitic': 'Vinson',
+    'Australian': 'Puncak Jaya',
+    'Eurasian' : 'Everest',
+    'North_American': 'Denali',
+    'Pacific': 'Mauna Kea',
+    'South_American': 'Aconcagua'
+}
+
+# Create a statement that prints forth nearest star
+
+print(stars[3])
+
+# Create a statement that prints the highest peak on the Pacific plate
+
+print(peaks['Pacific'])
